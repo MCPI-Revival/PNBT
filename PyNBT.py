81ea1f9b32200c997ef7e2e23e8b766a7d1ebbb3
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-02-21 00:41:36 +0200

    Update PyNBT.py

--- PyNBT.py
+++ PyNBT.py
@@ -17,61 +17,52 @@ tag = {
     "long_array": 12
 }
 
-data = b""
-offset = 0
+stream = {
+    "data": b"",
+    "offset": 0
+}
+
 nbt = {}
 endianess = "<"
 
-def reset():
-    data = b""
-    offset = 0
-    nbt = {}
+def read_stream(length):
+    stream["offset"] += length
+    return stream["data"][offset - length:offset]
 
-def read():
-    while not len(data) <= offset:
-        tag_type = struct.unpack("b", data[offset:offset + 1])[0]
-        offset += 1
-        if tag_type == tag["end"]:
-            break
-        tag_name_length = struct.unpack(endianess + "H", data[offset:offset + 2])[0]
+def read(nbt_type):
+    if nbt_type == tag["byte"]:
+        tag_value = struct.unpack("b", read_stream(1))[0]
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["short"]:
+        tag_value = struct.unpack(endianess + "h", read_stream(2))[0]
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["int"]:
+        tag_value = struct.unpack(endianess + "l", data[offset:offset + 4])[0]
+        offset += 4
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["long"]:
+        tag_value = struct.unpack(endianess + "q", data[offset:offset + 8])[0]
+        offset += 8
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["float"]:
+        tag_value = struct.unpack(endianess + "f", data[offset:offset + 4])[0]
+        offset += 4
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["double"]:
+        tag_value = struct.unpack(endianess + "d", data[offset:offset + 8])[0]
+        offset += 8
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["byte_array"]:
+        byte_count = struct.unpack(endianess + "l", data[offset:offset + 4])[0]
+        offset += 4
+        tag_value = []
+        for i in range(0, byte_count):
+            tag_value.append(struct.unpack("b", data[offset:offset + 1])[0])
+            offset += 1  
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
+    elif nbt_type == tag["string"]:
+        string_length = struct.unpack(endianess + "H", data[offset:offset + 2])[0]
         offset += 2
-        tag_name = data[offset:offset + tag_name_length]
-        offset += tag_name_length
-        if nbt_type == tag["byte"]:
-            tag_value = struct.unpack("b", data[offset:offset + 1])[0]
-            offset += 1
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["short"]:
-            tag_value = struct.unpack(endianess + "h", data[offset:offset + 2])[0]
-            offset += 2
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["int"]:
-            tag_value = struct.unpack(endianess + "l", data[offset:offset + 4])[0]
-            offset += 4
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["long"]:
-            tag_value = struct.unpack(endianess + "q", data[offset:offset + 8])[0]
-            offset += 8
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["float"]:
-            tag_value = struct.unpack(endianess + "f", data[offset:offset + 4])[0]
-            offset += 4
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["double"]:
-            tag_value = struct.unpack(endianess + "d", data[offset:offset + 8])[0]
-            offset += 8
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["byte_array"]:
-            byte_count = struct.unpack(endianess + "l", data[offset:offset + 4])[0]
-            offset += 4
-            tag_value = []
-            for i in range(0, byte_count):
-                tag_value.append(struct.unpack("b", data[offset:offset + 1])[0])
-                offset += 1  
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
-        elif nbt_type == tag["string"]:
-            string_length = struct.unpack(endianess + "H", data[offset:offset + 2])[0]
-            offset += 2
-            tag_value = data[offset:offset + string_length]
-            offset += string_length
-            nbt[tag_name] = {"type": tag_type, "value": tag_value}
+        tag_value = data[offset:offset + string_length]
+        offset += string_length
+        nbt[tag_name] = {"type": tag_type, "value": tag_value}
